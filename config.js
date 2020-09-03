0f446f38ac410132aae0838d225a285a22ecb303
xcgx <36234354+xcgx@users.noreply.github.com>
2020-09-03 21:53:36 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const LOG_DELAY = 5000 // 日志输出时间间隔，单位毫秒
 const PAGE_SIZE = 1000 // 每次网络请求读取目录下的文件数，数值越大，越有可能超时，不得超过1000
 
 const RETRY_LIMIT = 7 // 如果某次请求失败，允许其重试的最大次数
-const PARALLEL_LIMIT = 100 // 网络请求的并行数量，可根据网络环境调整
+const PARALLEL_LIMIT = 1000 // 网络请求的并行数量，可根据网络环境调整
 
 const DEFAULT_TARGET = '1DMFoNjcoX_Z01u4_ojugQHgCvHaTe835' // 必填，拷贝默认目的地ID，如果不指定target，则会复制到此处，建议填写团队盘ID
 
@@ -17,8 +17,8 @@ const AUTH = { // 如果您拥有service account的json授权文件，可将其�
   refresh_token: 'your_refrest_token',
   expires: 0, // 可以留空
   access_token: '', // 可以留空
-  tg_token: 'bot_token', // 你的 telegram robot 的 token，获取方法参见 https://core.telegram.org/bots#6-botfather
-  tg_whitelist: ['your_tg_username'] // 你的tg username(t.me/username)，bot只会执行这个列表里的用户所发送的指令
+  tg_token: '1152212327:AAHX1YFqM4bEllnpuxYZu770YggmS8L43YY', // 你的 telegram robot 的 token，获取方法参见 https://core.telegram.org/bots#6-botfather
+  tg_whitelist: ['krisjess'] // 你的tg username(t.me/username)，bot只会执行这个列表里的用户所发送的指令
 }
 
 module.exports = { AUTH, PARALLEL_LIMIT, RETRY_LIMIT, TIMEOUT_BASE, TIMEOUT_MAX, LOG_DELAY, PAGE_SIZE, DEFAULT_TARGET }
